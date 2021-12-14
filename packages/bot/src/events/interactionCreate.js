c681420170f3d0449a44fac6e832f6d2076093b2
Alejandra Guadalupe L. Altamirano <pandita420@MacBook-Pro-de-Alejandra.local>
2021-12-14 17:59:57 -0500

    :sparkles: getting roles in interaction
    interaction create editing and getting roles

--- packages/bot/src/events/interactionCreate.js
+++ packages/bot/src/events/interactionCreate.js
@@ -4,11 +4,17 @@ const config = require('../config')
 module.exports = {
     name:"interactionCreate",
     on:true,
-    async execute(interaction,client){
+    async execute(interaction,client,member){
 
         if(interaction.isButton()){
             if(interaction.customId == 'interesado'){
-                
+                let userId = interaction.user.id;
+                let guild = client.guilds.cache.get("920091691896143932");
+                let member = await guild.members.fetch(userId);
+                console.log(member._roles);
+                console.log({roleslist: guild.roles})
+                let roles = guild.roles.cache.filter(r => member._roles.includes(r.id));
+                console.log({findroles: roles})
             }
             if(interaction.customId == 'no'){
                 interaction.reply("No te preocupes 😅, cuando gustes avísame con el comando `/search` cuando necesites algún empleo relacionado con tu ROL.");
