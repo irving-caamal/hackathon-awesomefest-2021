9cca72cfa5f77ce9c56b41a275aa37c9a68e0d77
irvv17 <irvv.17@gmail.com>
2021-12-13 23:40:25 -0500

    fix: add embed message

--- packages/bot/src/index.js
+++ packages/bot/src/index.js
@@ -1,7 +1,8 @@
 const config = require('./config')
 const Discord  = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
-
+const {searchJobs} = require('./services/getOnBoardApi')
 const { Collection, Intents } = require("discord.js");
 
 const fs = require('fs');
@@ -25,21 +26,6 @@ for(const file of eventFiles){
 }
 
 //curl --location -g --request GET 'https://www.getonbrd.com/api/v0/search/jobs?query=Ruby+on+Rails&per_page=2&page=1&expand=[%22company%22]'
-const base_url = 'https://sandbox.getonbrd.dev/api/v0'
-function searchJobs(query = 'php', per_page = 2, page = 1, expand = ['company']) {
-    console.log({query})
-    return fetch(`${base_url}/search/jobs?query=${query}&per_page=2&page=1&expand=["company"]`)
-    .then(res=> {
-        return res.json()
-    })
-    .then(data => {
-        return data;
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
 
 client.commands = new Collection();
 
@@ -57,17 +43,57 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async (message) => { 
-   if(message.content.startsWith('/')) {
-       let queryLength = message.content.split(' ')
-        let query = queryLength.splice(1, queryLength.length - 1).join(' ')
-       const jobs = await searchJobs(query)
-       console.log({data: jobs.data})
-       jobs.data.map(job => {
-        message.reply(job.id);
-       })
-       
-   }
-});
+    if(message.content.startsWith('/')) {
+        let queryLength = message.content.split(' ')
+         let query = queryLength.splice(1, queryLength.length - 1).join(' ')
+        const jobs = await searchJobs(query)
+        if (!jobs.data) {
+         message.reply('Sorry there are no jobs matching your search');
+        }
+        const embeds = []
+        jobs.data.map((job) => {
+        
+            const newEmbed = {
+                color: 0x0099ff,
+                title: job.attributes.title,
+                url: job.links.public_url,
+                author: {
+                    name: job.attributes.company.data.attributes.name,
+                    icon_url: job.attributes.company.data.attributes.logo,
+                    url: job.attributes.company.data.attributes.web,
+                },
+                description: job.attributes.description.replaceAll('<p>', '\n').replaceAll('</p>', '\n').replaceAll('</li><li>', '\n').replaceAll('<ul><li>', '\n').replaceAll('</li></ul>', '\n').replaceAll('&amp;', 'and'),
+                thumbnail: {
+                    url: job.attributes.company.data.attributes.logo,
+                },
+                fields: [
+                    {
+                        name: 'Salary from',
+                        value: job.attributes.min_salary ? `${job.attributes.min_salary}` : 'Not available',
+                        inline: true,
+                    },
+                    {
+                        name: 'Country',
+                        value: job.attributes.country,
+                        inline: true,
+                    },
+                    {
+                        name: 'Categoria',
+                        value: job.attributes.category_name,
+                        inline: true,
+                    },
+                ],
+                timestamp: new Date(job.attributes.published_at),
+                footer: {
+                    text: 'Some footer text here',
+                },
+            };
+            embeds.push(newEmbed)
+        })
+        
+        message.channel.send({embeds: embeds});
+    }
+ });
 
 console.log({config})
 client.login(config.DISCORD_TOKEN);
